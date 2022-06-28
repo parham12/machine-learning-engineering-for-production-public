e839fc21d3f83fc1bc29443d4c384700d726ff49
parham12 <plavigne19@gmail.com>
2022-06-28 15:10:24 +0430

    Test te new test 3

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/test_clf.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/test_clf.py
@@ -1,20 +1,5 @@
 import pickle
 from main import clf
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler
-
-
-def test_pipeline_and_scaler():
-
-    # Check if clf is an instance of sklearn.pipeline.Pipeline 
-    isPipeline = isinstance(clf, Pipeline)
-    assert isPipeline
-    
-    if isPipeline:
-        # Check if first step of pipeline is an instance of 
-        # sklearn.preprocessing.StandardScaler
-        firstStep = [v for v in clf.named_steps.values()][0]
-        assert isinstance(firstStep, StandardScaler)
 
 #test test
 def test_accuracy():
